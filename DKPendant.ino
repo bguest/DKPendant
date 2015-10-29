1c92bbba674b3b696977cd8b1a1d2e3d9be7f724
Benjamin Guest <benguest@gmail.com>
2015-10-29 00:04:16 -0400

    Experiment with PWM in ATmeg328

--- DKPendant.ino
+++ DKPendant.ino
@@ -1,14 +1,18 @@
+/*#define __AVR_ATtiny85__*/
+#define __AVR_ATmega328P__
+
 #include <Bounce2.h>
 #include <Pendant.h>
 #include <Adafruit_NeoPixel.h>
 
-#ifdef __AVR__
+#ifdef __AVR_ATtiny85__
   #include <avr/power.h>
 #endif
 
-#define __AVR_ATtiny85__
+#ifndef __AVR_ATtiny85__
 #ifndef __AVR_ATtiny85__
   #define DEBUG
+#endif
 #endif
 
 Pendant pendant;
--- lib/IRSend.cpp
+++ lib/IRSend.cpp
@@ -10,8 +10,27 @@
 #define SAMSUNG_ZERO_SPACE   560
 #define SAMSUNG_RPT_SPACE   2250
 
-#define MARK  (TCCR1 |= _BV(COM1A1)); // Enable pin 6 (PB1) PWM output
-#define SPACE (TCCR1 &= ~(_BV(COM1A1))); // Disable pin 6 (PB1) PWM output
+#ifdef F_CPU
+#	define SYSCLOCK  F_CPU     // main Arduino clock
+#else
+#	define SYSCLOCK  16000000  // main Arduino clock
+#endif
+
+#ifdef __AVR_ATmega328P__
+  #define MARK   (TCCR1A |= _BV(COM1A1));
+  #define SPACE  (TCCR1A &= ~(_BV(COM1A1)));
+#else
+  #define MARK  (TCCR1 |= _BV(COM1A1)); // Enable pin 6 (PB1) PWM output
+  #define SPACE (TCCR1 &= ~(_BV(COM1A1))); // Disable pin 6 (PB1) PWM output
+#endif
+
+#define TIMER_CONFIG_KHZ(val) ({ \
+	const uint16_t pwmval = SYSCLOCK / 2000 / (val); \
+	TCCR1A                = _BV(WGM11); \
+	TCCR1B                = _BV(WGM13) | _BV(CS10); \
+	ICR1                  = pwmval; \
+	OCR1A                 = pwmval / 3; \
+})
 
 IRSend::IRSend(){
 }
@@ -57,9 +76,13 @@ void IRSend::init(){
   //ADCSRA &= ~(_BV(ADEN));
 
   /* Enable PWM with a frequency of 38kHz on pin 6 (OC1A) */
+#ifdef __AVR_ATtiny85__
   TCCR1 = _BV(PWM1A) | _BV(CS12); // Enable PWM and set prescaler to 8
   OCR1C = (((F_CPU/8/PWMFREQUENCY))-1); // Set PWM Frequency to 38kHz, OCR1C is TOP, see the datasheet page 90
   OCR1A = OCR1C/3; // 33% duty cycle
+#else
+  TIMER_CONFIG_KHZ(38);
+#endif
 
   sei(); // Enables interrupts by setting the global interrupt mask
 }
